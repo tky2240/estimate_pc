3a484319073bd7ba4148a826511a93e46fe46dcf
tky2240 <tky2240@gmail.com>
2024-04-03 01:14:18 +0900

    fix fetching motherboard, gpu, case and power supply info

--- kakaku_database/scraping_kakaku/main.py
+++ kakaku_database/scraping_kakaku/main.py
@@ -31,27 +31,51 @@ def run(csv_path, json_path):
         item_data_dictionary = dict()
         for genre in reader:
             print(f"name: {genre['name']} code: {genre['code']}")
-            # if genre["name"] != "power_supply":
+            # if genre["name"] != "motherboard":
             #     continue
-            time.sleep(random.uniform(2, 4))
-            current_genre_url = urllib.parse.urljoin(base_url, genre['code'])
+            time.sleep(random.uniform(2, 10))
+            current_genre_url = urllib.parse.urljoin(base_url, genre['code'] + "/")
+            print(current_genre_url)
             response = None
+
             if genre["name"] == "case":
-                response = requests.post(current_genre_url, data={
-                    "DispSaleDate": "on", "DispNonPrice": "on", "Sort": "price_asc", "CategoryCD": genre['code'], "DispTypeColor": 2, "ButtonType": "D"})
+                response = requests.post(
+                    current_genre_url, 
+                    data={
+                        "DispSaleDate": "on", "DispNonPrice": "on", "Sort": "price_asc", "CategoryCD": genre['code'], "DispTypeColor": 2, "ButtonType": "D"
+                    },
+                )
 
             elif genre["name"] == "memory":
-                response = requests.post(current_genre_url, data={
-                    "DispSaleDate": "on", "DispNonPrice": "on", "Sort": "price_asc", "CategoryCD": genre['code'], "MemoryClass": "7,6"})
+                response = requests.post(
+                    current_genre_url, 
+                    data={
+                        "DispSaleDate": "on", "DispNonPrice": "on", "Sort": "price_asc", "CategoryCD": genre['code'], "MemoryClass": "7,6"
+                    },
+                )
+
             elif genre["name"] == "gpu":
-                response = requests.post(current_genre_url, data={
-                    "DispSaleDate": "on", "DispNonPrice": "on", "Sort": "price_asc", "CategoryCD": genre['code'], "RAM": 1024})
+                response = requests.post(
+                    current_genre_url, 
+                    data={
+                        "DispSaleDate": "on", "DispNonPrice": "on", "Sort": "price_asc", "CategoryCD": genre['code'], "RAM": 1024
+                    },
+                )
+                
             elif genre["name"] == "ssd":
-                response = requests.post(current_genre_url, data={
-                    "DispSaleDate": "on", "DispNonPrice": "on", "Sort": "price_asc", "CategoryCD": genre['code'], "FormFactor": "2,6,8"})
+                response = requests.post(
+                    current_genre_url, 
+                    data={
+                        "DispSaleDate": "on", "DispNonPrice": "on", "Sort": "price_asc", "CategoryCD": genre['code'], "FormFactor": "2,6,8"
+                    },
+                )
             else:
-                response = requests.post(current_genre_url, data={
-                    "DispSaleDate": "on", "DispNonPrice": "on", "Sort": "price_asc", "CategoryCD": genre['code']})
+                response = requests.post(
+                    current_genre_url, 
+                    data={
+                        "DispSaleDate": "on", "DispNonPrice": "on", "Sort": "price_asc", "CategoryCD": genre['code']
+                    },
+                )
 
             if not response.ok:
                 print("post failed")
@@ -62,16 +86,17 @@ def run(csv_path, json_path):
                     "div.alignL > span.number").contents[0].text.replace(",", ""))
             except:
                 print("item count parse error")
-                time.sleep(random.uniform(2, 4))
+                time.sleep(random.uniform(2, 10))
                 continue
             page_count = int(item_count / 30) + 1
             print(f"1 / {page_count}...")
             item_tables = [soup.select_one(
-                "table.alignC.tblBorderGray02.mTop5")]
+                "#spec_result > div.alignC > form > table > tr:nth-child(3) > td > table")]
 
             for i in range(2, page_count+1):
+                # break
                 print(f"{i} / {page_count}...")
-                time.sleep(random.uniform(1, 3))
+                time.sleep(random.uniform(2, 10))
                 if not response.ok:
                     print("post failed")
                     continue
@@ -230,6 +255,9 @@ def update_motherboard_information(item_tables: List[Tag]):
         for table_row in table_rows:
             try:
                 table_data: List[Tag] = table_row.find_all('td')
+                # for i, table in enumerate(table_data):
+                #     print(i, table)
+                # break
                 motherboard_data_dictionary = {
                     "item_id": re.findall(r"/item/(\w+)/", table_data[1].find('a').get('href'))[0],
                     "name": table_data[2].text.strip(),
@@ -243,14 +271,14 @@ def update_motherboard_information(item_tables: List[Tag]):
                     "memory_type": table_data[12].text.strip(),
                     "memory_slot_count": int(table_data[13].text.strip()),
                     "max_memory_capacity": int(re.findall(r"(\d+)GB", table_data[14].text.strip())[0]),
-                    "pci_slot_count": 0 if table_data[15].text.strip() == "" else int(re.findall(r"(\d+)本", table_data[15].text.strip())[0]),
-                    "pcie_x16_slot_count": 0 if table_data[16].text.strip() == "" else int(re.findall(r"(\d+)本", table_data[16].text.strip())[0]),
-                    "pcie_x8_slot_count": 0 if table_data[17].text.strip() == "" else int(re.findall(r"(\d+)本", table_data[17].text.strip())[0]),
-                    "pcie_x4_slot_count": 0 if table_data[18].text.strip() == "" else int(re.findall(r"(\d+)本", table_data[18].text.strip())[0]),
-                    "pcie_x1_slot_count": 0 if table_data[19].text.strip() == "" else int(re.findall(r"(\d+)本", table_data[19].text.strip())[0]),
+                    "pci_slot_count": 0 if table_data[19].text.strip() == "" else int(re.findall(r"(\d+)本", table_data[19].text.strip())[0]),
+                    "pcie_x16_slot_count": 0 if table_data[15].text.strip() == "" else int(re.findall(r"(\d+)x PCI Express", table_data[15].text.strip())[0]),
+                    "pcie_x8_slot_count": 0 if table_data[16].text.strip() == "" else int(re.findall(r"(\d+)x PCI Express", table_data[16].text.strip())[0]),
+                    "pcie_x4_slot_count": 0 if table_data[17].text.strip() == "" else int(re.findall(r"(\d+)x PCI Express", table_data[17].text.strip())[0]),
+                    "pcie_x1_slot_count": 0 if table_data[18].text.strip() == "" else int(re.findall(r"(\d+)x PCI Express", table_data[18].text.strip())[0]),
                     "sata_connector_count": 0 if table_data[20].text.strip() == "" else int(re.findall(r"(\d+)本", table_data[20].text.strip())[0]),
-                    "release_date": None if table_data[40].text.strip() == "" else datetime.datetime.strptime(re.sub(r"\s", "", table_data[40].text), "%Y年%m月%d日").date()
-                    if re.match(r"\d+年\d+月\d+日", re.sub(r"\s", "", table_data[40].text)) is not None else datetime.datetime.strptime(re.findall(r"(\d+年\d+月)", re.sub(r"\s", "", table_data[40].text))[0], "%Y年%m月").date(),
+                    "release_date": None if table_data[42].text.strip() == "" else datetime.datetime.strptime(re.sub(r"\s", "", table_data[42].text), "%Y年%m月%d日").date()
+                    if re.match(r"\d+年\d+月\d+日", re.sub(r"\s", "", table_data[42].text)) is not None else datetime.datetime.strptime(re.findall(r"(\d+年\d+月)", re.sub(r"\s", "", table_data[42].text))[0], "%Y年%m月").date(),
                     "is_exist": True
                 }
             except Exception as exception:
@@ -319,17 +347,17 @@ def update_gpu_information(item_tables: List[Tag]):
                     "pcie_interface": table_data[14].text.strip(),
                     "is_low_profile": table_data[15].text.strip() != "",
                     "cooling_solution": table_data[16].text.strip(),
-                    "tdp": None if re.match(r"(\d+)W", table_data[17].text.strip()) is None else int(re.findall(r"(\d+)W", table_data[17].text.strip())[0]),
-                    "hdmi_count": 0 if re.match(r"(\d+)ポート", table_data[23].text.strip()) is None else int(re.findall(r"(\d+)ポート", table_data[23].text.strip())[0]),
-                    "displayport_count": 0 if re.match(r"(\d+)ポート", table_data[24].text.strip()) is None else int(re.findall(r"(\d+)ポート", table_data[24].text.strip())[0]),
-                    "width": None if table_data[25].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[25].text.strip())[0][0]),
-                    "height": None if table_data[25].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[25].text.strip())[0][1]),
-                    "depth": None if table_data[25].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[25].text.strip())[0][2]),
-                    "radiator_width": None if table_data[26].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[26].text.strip())[0][0]),
-                    "radiator_height": None if table_data[26].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[26].text.strip())[0][1]),
-                    "radiator_depth": None if table_data[26].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[26].text.strip())[0][2]),
-                    "release_date": None if table_data[27].text.strip() == "" else datetime.datetime.strptime(re.sub(r"\s", "", table_data[27].text), "%Y年%m月%d日").date()
-                    if re.match(r"\d+年\d+月\d+日", re.sub(r"\s", "", table_data[27].text)) is not None else datetime.datetime.strptime(re.findall(r"(\d+年\d+月)", re.sub(r"\s", "", table_data[27].text))[0], "%Y年%m月").date(),
+                    "tdp": None if re.match(r"(\d+)W", table_data[19].text.strip()) is None else int(re.findall(r"(\d+)W", table_data[19].text.strip())[0]),
+                    "hdmi_count": 0 if re.match(r"(\d+)ポート", table_data[26].text.strip()) is None else int(re.findall(r"(\d+)ポート", table_data[26].text.strip())[0]),
+                    "displayport_count": 0 if re.match(r"(\d+)ポート", table_data[27].text.strip()) is None else int(re.findall(r"(\d+)ポート", table_data[27].text.strip())[0]),
+                    "width": None if table_data[28].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[28].text.strip())[0][0]),
+                    "height": None if table_data[28].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[28].text.strip())[0][1]),
+                    "depth": None if table_data[28].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[28].text.strip())[0][2]),
+                    "radiator_width": None if table_data[29].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[29].text.strip())[0][0]),
+                    "radiator_height": None if table_data[29].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[29].text.strip())[0][1]),
+                    "radiator_depth": None if table_data[29].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[29].text.strip())[0][2]),
+                    "release_date": None if table_data[31].text.strip() == "" else datetime.datetime.strptime(re.sub(r"\s", "", table_data[31].text), "%Y年%m月%d日").date()
+                    if re.match(r"\d+年\d+月\d+日", re.sub(r"\s", "", table_data[31].text)) is not None else datetime.datetime.strptime(re.findall(r"(\d+年\d+月)", re.sub(r"\s", "", table_data[31].text))[0], "%Y年%m月").date(),
                     "is_exist": True
                 }
             except Exception as exception:
@@ -424,13 +452,13 @@ def update_case_information(item_tables: List[Tag]):
                     "max_power_supply_size": None if re.match(r"(\d+)mm", re.sub(r"[^0-9m]", "", table_data[11].text.strip())) is None else int(re.findall(r"(\d+)mm", re.sub(r"[^0-9m]", "", table_data[11].text.strip()))[0]),
                     "slot_count": None if re.match(r"(\d)", table_data[13].text.strip()) is None else int(re.findall(r"(\d)", table_data[13].text.strip())[0]),
                     "drive_bay_information": table_data[14].get_text("\n").strip(),
-                    "is_low_profile": table_data[17].text.strip() != "",
-                    "weight": None if table_data[19].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)kg", table_data[19].text.strip())[0]),
-                    "width": None if table_data[20].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[20].text.strip())[0][0]),
-                    "height": None if table_data[20].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[20].text.strip())[0][1]),
-                    "depth": None if table_data[20].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[20].text.strip())[0][2]),
-                    "release_date": None if table_data[23].text.strip() == "" else datetime.datetime.strptime(re.sub(r"\s", "", table_data[23].text), "%Y年%m月%d日").date()
-                    if re.match(r"\d+年\d+月\d+日", re.sub(r"\s", "", table_data[23].text)) is not None else datetime.datetime.strptime(re.findall(r"(\d+年\d+月)", re.sub(r"\s", "", table_data[23].text))[0], "%Y年%m月").date(),
+                    "is_low_profile": table_data[18].text.strip() != "",
+                    "weight": None if table_data[20].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)kg", table_data[20].text.strip())[0]),
+                    "width": None if table_data[21].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[21].text.strip())[0][0]),
+                    "height": None if table_data[21].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[21].text.strip())[0][1]),
+                    "depth": None if table_data[21].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[21].text.strip())[0][2]),
+                    "release_date": None if table_data[25].text.strip() == "" else datetime.datetime.strptime(re.sub(r"\s", "", table_data[25].text), "%Y年%m月%d日").date()
+                    if re.match(r"\d+年\d+月\d+日", re.sub(r"\s", "", table_data[25].text)) is not None else datetime.datetime.strptime(re.findall(r"(\d+年\d+月)", re.sub(r"\s", "", table_data[25].text))[0], "%Y年%m月").date(),
                     "is_exist": True
                 }
                 motherboard_form_factors = []
@@ -477,8 +505,8 @@ def update_power_supply_information(item_tables: List[Tag]):
                     "width": None if table_data[26].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[26].text.strip())[0][0]),
                     "height": None if table_data[26].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[26].text.strip())[0][1]),
                     "depth": None if table_data[26].text.strip() == "" else float(re.findall(r"(\d+\.\d+|\d+)x(\d+\.\d+|\d+)x(\d+\.\d+|\d+)", table_data[26].text.strip())[0][2]),
-                    "release_date": None if table_data[28].text.strip() == "" else datetime.datetime.strptime(re.sub(r"\s", "", table_data[28].text), "%Y年%m月%d日").date()
-                    if re.match(r"\d+年\d+月\d+日", re.sub(r"\s", "", table_data[28].text)) is not None else datetime.datetime.strptime(re.findall(r"(\d+年\d+月)", re.sub(r"\s", "", table_data[28].text))[0], "%Y年%m月").date(),
+                    "release_date": None if table_data[29].text.strip() == "" else datetime.datetime.strptime(re.sub(r"\s", "", table_data[29].text), "%Y年%m月%d日").date()
+                    if re.match(r"\d+年\d+月\d+日", re.sub(r"\s", "", table_data[29].text)) is not None else datetime.datetime.strptime(re.findall(r"(\d+年\d+月)", re.sub(r"\s", "", table_data[29].text))[0], "%Y年%m月").date(),
                     "is_exist": True
                 }
             except Exception as exception:
